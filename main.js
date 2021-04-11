57905b96fe13e2b8302e9b6d3c71180dbb8f92af
OakBricks <oakbricks@gmail.com>
2021-04-10 23:21:35 -0500

    Init

---
+++ main.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json');
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === config.prefix + 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === config.prefix + 'help') {
+    msg.channel.send('Read the docs at:');
+  }
+});
+
+client.login(config.token);
