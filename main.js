33ed884a189396e91e879a41558801aa8ce1a5ac
OakBricks <oakbricks@gmail.com>
2021-04-11 10:21:41 -0500

    Add some more info to info command, update version to 1.0.0.1 instead of 1.0.0-ng

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const verfile = require('./verfile.json');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Version is ` + verfile.version + '!');
+  console.log('just a quick reminder to please use nodemon for automatic reloads and debugging!');
+  console.log('Some info about your config!');
+  console.log('bot_token: ' + config.token);
+  console.log('supportsite: ' + config.suppportsite);
+  console.log('prefix: ' + config.prefix)
 });
 
 client.on('message', msg => {
@@ -30,6 +36,7 @@ client.on('message', msg => {
     msg.channel.send('Version: ' + verfile.version);
     msg.channel.send('Prefix: ' + config.prefix);
     msg.channel.send('Prefix Example. ' + config.prefix + 'help');
+    msg.channel.send('Support Website: ' + config.suppportsite);
   }
 });
 
