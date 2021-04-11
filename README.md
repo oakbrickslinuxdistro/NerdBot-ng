11654dcfe11ce07b49080032ef9baf6f2140d978
OakBricks <oakbricks@gmail.com>
2021-04-11 00:01:57 -0500

    Improve on the bot more

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # NerdBot-ng!
 ##### Yet another discord bot that does nothing!
 
+## Setting the darn thing up!
+1.1. Create a file called "config.json" where "main.js" is located!
+    Syntax for "config.json" should look like:
+        ```
+            {
+                "token": "BOT_TOKEN",
+                "prefix": "Choose a prefix like: nerd! or n! check the wiki for more info",
+            }
+        ```
+
+1.2. if you have windows just install node.js and run the start-dbg.bat file!
+
 ###### I am not responisble for 1. Thermonuclear war 2. You getting fired because the bot kept pinging you on mobile 3. Your PC or VPS blowing up!
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
+const verfile = require('./verfile.json');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -21,6 +22,14 @@ client.on('message', msg => {
 client.on('message', msg => {
   if (msg.content === config.prefix + 'why') {
     msg.channel.send('joe nuts');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === config.prefix + 'info') {
+    msg.channel.send('Version: ' + verfile.version);
+    msg.channel.send('Prefix: ' + config.prefix);
+    msg.channel.send('Prefix Example. ' + config.prefix + 'help');
   }
 });
 
